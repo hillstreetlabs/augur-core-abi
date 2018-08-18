d9bc72c0e610eb08c50644d5df5d339816df8fb5
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-08-18 16:16:51 -0700

    Also export addresses from augur.js

--- build.js
+++ build.js
@@ -1,17 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 const abi = require("augur-core").abi;
+const Augur = require("Augur.js");
 
-const keys = Object.keys(abi);
+const augur = new Augur();
+const addresses = augur.contracts.addresses;
 
 let es2015Content = "";
 let commonjsContent = `"use strict";\nvar exports = module.exports = {};\n`;
 
+const keys = Object.keys(abi);
 for (let i = 0; i < keys.length; i++) {
   const key = keys[i];
   es2015Content += `export const ${key} = ${JSON.stringify(abi[key])};\n`;
   commonjsContent += `exports.${key} = ${JSON.stringify(abi[key])};\n`;
 }
+
+// Add addresses
+es2015Content += `export const addresses = ${JSON.stringify(addresses)};\n`;
+commonjsContent += `exports.addresses = ${JSON.stringify(addresses)};\n`;
 
 fs.writeFileSync(path.join(__dirname, "es2015.js"), es2015Content);
 fs.writeFileSync(path.join(__dirname, "commonjs.js"), commonjsContent);
