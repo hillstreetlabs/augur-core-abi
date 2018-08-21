639350eab5b78f40a440487630ebd38fb1e42c25
Graham Kaemmer <graham.kaemmer@gmail.com>
2018-08-21 11:39:39 -0700

    Add typings

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 All Augur ABIs, without any dependencies and fully tree-shakeable.
 
+Also includes deployed Augur addresses, so you don't need to grab them from `augur.js`.
+
 ### Why?
 
 To use augur ABIs in your code you have to install augur-core, which has dozens of dependencies. Furthermore, the ABIs are quite large (140kb uncompressed), so if you only want to use one contract in your frontend, it's nice to allow webpack to tree-shake the unused ABIs.
@@ -9,5 +11,8 @@ To use augur ABIs in your code you have to install augur-core, which has dozens 
 To allow tree-shaking:
 
 ```
-import { CompleteSets } from "augur-core-abi";
+import { CompleteSets, addresses } from "augur-core-abi";
+
+const networkId = "1";
+new Contract(CompleteSets, addresses[networkId].CompleteSets);
 ```
--- build.js
+++ build.js
@@ -8,17 +8,23 @@ const addresses = augur.contracts.addresses;
 
 let es2015Content = "";
 let commonjsContent = `"use strict";\nvar exports = module.exports = {};\n`;
+let typingsContent = `declare module "augur-core-abi" {\n`;
 
 const keys = Object.keys(abi);
 for (let i = 0; i < keys.length; i++) {
   const key = keys[i];
   es2015Content += `export const ${key} = ${JSON.stringify(abi[key])};\n`;
   commonjsContent += `exports.${key} = ${JSON.stringify(abi[key])};\n`;
+  typingsContent += `  export const ${key}: any[];\n`;
 }
 
 // Add addresses
 es2015Content += `export const addresses = ${JSON.stringify(addresses)};\n`;
 commonjsContent += `exports.addresses = ${JSON.stringify(addresses)};\n`;
+typingsContent += `  export const addresses: { [networkId: string]: { [contractName: string]: string } };\n`;
+
+typingsContent += `}\n`;
 
 fs.writeFileSync(path.join(__dirname, "es2015.js"), es2015Content);
 fs.writeFileSync(path.join(__dirname, "commonjs.js"), commonjsContent);
+fs.writeFileSync(path.join(__dirname, "index.d.ts"), typingsContent);
